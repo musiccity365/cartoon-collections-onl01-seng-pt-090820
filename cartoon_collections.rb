274c83e972973dc88f89b9178e374119822f3dbf
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-17 23:18:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,13 @@ end
 
 def long_planeteer_calls(long_planteer_calls)# code an argument here
   # Your code here
-  
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
 end
 
 def find_the_cheese# code an argument here
